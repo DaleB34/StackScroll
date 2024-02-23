cccbd20bc4a458e16ecb55692895a76df45802e9
91112021 <dalebolido@gmail.com>
2024-02-23 13:14:10 -0800

    Declared and filled list

--- app/src/main/java/com/example/stackscroll/MainActivity.java
+++ app/src/main/java/com/example/stackscroll/MainActivity.java
@@ -10,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initialize a list with a few elements in it
+        //when adding or deleting an element, refresh the list
+        //to display/refresh the list, go through every node, and for each one, add a new TextView to the scrollview
+        StackList stack = new StackList();
+        stack.addTailNode(0);
+        stack.addTailNode(1);
+        stack.addTailNode(2);
+        stack.addTailNode(3);
+        stack.addTailNode(4);
+        stack.showList();
     }
 }
