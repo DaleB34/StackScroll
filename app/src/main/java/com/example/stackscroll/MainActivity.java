125c6cfe41cfa8d0d362cd3bfcf2f6e5adb6ea4f
91112021 <dalebolido@gmail.com>
2024-03-08 12:50:59 -0800

    Layout in ScrollView fills correctly

--- app/src/main/java/com/example/stackscroll/MainActivity.java
+++ app/src/main/java/com/example/stackscroll/MainActivity.java
@@ -26,25 +26,23 @@ public class MainActivity extends AppCompatActivity {
         //when adding or deleting an element, refresh the list
         //to display/refresh the list, go through every node, and for each one, add a new TextView to the scrollview
         StackList stack = new StackList();
-        stack.addTailNode(0);
+        stack.addTailNode(0);//head
         stack.addTailNode(1);
         stack.addTailNode(2);
         stack.addTailNode(3);
-        stack.addTailNode(4);
+        stack.addTailNode(4);//tail
 
 
         System.out.println(stack.makeSet());
-        int[] booch = stack.makeSet();
+        int[] data = stack.makeSet();
 
-        String blah = "";
-        for(int n: booch)
+        for(int i = 0; i < data.length; i++)
         {
-            blah+= n+"";
+            TextView tv = new TextView(this);
+            tv.setText(data[i] + "");
+            ll.addView(tv);
         }
 
-        TextView tv = new TextView(this);
-            tv.setText(blah);
-            ll.addView(tv);
 //        while( != null)
 //        {
 //            TextView tv = new TextView(this);
