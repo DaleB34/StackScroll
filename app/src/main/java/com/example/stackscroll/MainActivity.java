22506dfa002017211914eab5e97976abe137e17d
91112021 <dalebolido@gmail.com>
2024-03-11 13:19:33 -0700

    added onClick listener but it adds to bottom instead of top

--- app/src/main/java/com/example/stackscroll/MainActivity.java
+++ app/src/main/java/com/example/stackscroll/MainActivity.java
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Node;
+import org.w3c.dom.Text;
 
 import java.security.AccessController;
 
@@ -22,6 +25,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LinearLayout ll = findViewById(R.id.stackedList);
+        TextView input = findViewById(R.id.addText);
+        Button addBtn = findViewById(R.id.addBtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                TextView tv = new TextView(ll.getContext());
+                tv.setText(input.getText());
+                input.setText("");
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25f);
+                ll.addView(tv);
+            }
+
+        });
 
         //initialize a stack with a few elements in it
         StackList stack = new StackList();
